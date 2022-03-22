7f34f26327c4f8992680b78b1220ecccdd4aebd8
Antonio Sarno <a.sarno@wakala.it>
2022-03-22 09:44:16 +0100

    fix include

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # exercisesSO
 Operating Systems Exercises
+
+
--- user_identification/userIdentification.c
+++ user_identification/userIdentification.c
@@ -1,4 +1,4 @@
-#include "../apue.h"
+#include "./apue.h"
 int main(void){
     //stamp userID and groupID
     printf("uid = %d, gid = %d\n", getuid(), getgid());
