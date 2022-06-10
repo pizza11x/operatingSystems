95c8ed914f52a46dab653cdcae26d875fa4eef1e
Antonio Sarno <>
2022-06-11 00:19:30 +0200

    Some example completed

---
+++ shell_script/myFirstScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "my first script"
+# create a variable and stamp content
+today=saturday
+echo $today
+echo "script end"
---
+++ shell_script/read.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "digit:"
+read something
+echo "Hi: $something"
